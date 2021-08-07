85c41bb0d09e72bce8399ba1e2cad426ee1b1005
Chutiwat.C <arttartkhai@gmail.com>
2021-08-07 23:44:21 +0700

    add utilitity fn for check command

--- src/utils/message.test.ts
+++ src/utils/message.test.ts
@@ -1,8 +1,40 @@
-import { extractCommand } from './message';
+import { extractCommand } from './message'
 
 describe('extractCommand fn', () => {
-  const input = 'price btc';
-  it('should return command', () => {
-    expect(extractCommand(input)).toEqual('price');
-  });
-});
+  it('should return command with no args', () => {
+    let input
+
+    input = '>price'
+    expect(extractCommand(input)).toEqual({
+      command: 'price',
+      args: [],
+    })
+
+    input = '>price    '
+    expect(extractCommand(input)).toEqual({
+      command: 'price',
+      args: [],
+    })
+  })
+
+  it('should return command with correct args', () => {
+    let input
+    
+    input = '>price btc eth'
+    expect(extractCommand(input)).toEqual({
+      command: 'price',
+      args: ['btc', 'eth'],
+    })
+
+    input = '>price btc           eth'
+    expect(extractCommand(input)).toEqual({
+      command: 'price',
+      args: ['btc', 'eth'],
+    })
+  })
+  
+  it('should return nothing', () => {
+    let input = 'price'
+    expect(extractCommand(input)).toBeUndefined()
+  })
+})
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,4 +1,26 @@
-export const extractCommand = (message : string) : string => {
-    //TODO: use regex
-    return message.split(' ')[0]
+import * as _ from 'lodash';
+interface ICommandResult {
+  command: string;
+  args: string[];
 }
+
+export const extractCommand = (message: string): ICommandResult | undefined => {
+  // command with args
+  const regex: RegExp = />(\w+)\s(.+)/;
+  //TODO: and word is in command list
+  if (regex.test(message)) {
+    const matchGroups = regex.exec(message);
+    if (matchGroups) {
+      const command = matchGroups[1].toLowerCase();
+      const argsString = matchGroups[2];
+      const args = argsString.split(' ').filter((a) => !!a);
+      return { command, args };
+    }
+  }
+
+  // pure command
+  //TODO: and word is in command list
+  if (_.startsWith(message, '>')) {
+    return { command: message.replace('>', '').trim().toLowerCase(), args: [] };
+  }
+};
